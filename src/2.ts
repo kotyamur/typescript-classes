7e86edd9098a555dbd775a299abdd13b8b9d3812
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-20 19:56:53 +0300

    Update 2.ts

--- src/2.ts
+++ src/2.ts
@@ -16,9 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+  public name: string;
+  private department: string;
+  protected salary: number;
 
   constructor(name: string, department: string, salary: number) {
     this.name = name;
@@ -33,7 +33,7 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(salary: number, department: string, name: string) {
+    super(name, department, salary + 10000);
+  }
 }
-
-
-export {};
