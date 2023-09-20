b0ef0637453f415a3dd7e9ff18b9940abaec5ec2
Kateryna Kleshch <ketrins1993@gmail.com>
2023-09-20 22:33:21 +0300

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,47 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(objKey: Key) {
+    this.key = objKey;
+  }
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean = false;
+  key: Key;
+  tenants: object[] = [];
+  comeIn(obP: Person) {
+    if (this.door) {
+      this.tenants.push(obP);
+    }
+  }
+  abstract openDoor(objKey:Key);
+}
+
+class MyHouse extends House {
+  constructor(objKey: Key) {
+    super();
+    this.key = objKey; 
+  }
+  openDoor(objKey: Key) {
+    if (objKey.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +50,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
